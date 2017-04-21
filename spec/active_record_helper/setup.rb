8bff6d744c99f19e835115211c443ac3f6db6c79
James Coleman <jtc331@gmail.com>
2017-04-21 15:37:40 -0300

    Support Rails 5. (#4)
    HT @agfor for some of this code.

--- spec/active_record_helper/setup.rb
+++ spec/active_record_helper/setup.rb
@@ -13,17 +13,17 @@ ActiveRecord::Base.configurations = {
 }
 
 env = ENV['DATABASE'] ||= 'sqlite'
+config = ActiveRecord::Base.configurations[env]
+
 case env
 when 'postgresql'
   ActiveRecord::Tasks::DatabaseTasks.instance_variable_set('@env', env)
-  config = ActiveRecord::Base.configurations[env]
-
   ActiveRecord::Tasks::DatabaseTasks.drop_current
   ActiveRecord::Tasks::DatabaseTasks.create_current
   ActiveRecord::Tasks::DatabaseTasks.load_schema_current(:ruby, File.expand_path('../schema.rb', __FILE__))
-  ActiveRecord::Base.establish_connection(env)
+  ActiveRecord::Base.establish_connection(config)
 when 'sqlite'
-  ActiveRecord::Base.establish_connection(env)
+  ActiveRecord::Base.establish_connection(config)
   require_relative 'schema'
 else
   raise ArgumentError, 'Unrecognized ENV["DATABASE"] argument.'
